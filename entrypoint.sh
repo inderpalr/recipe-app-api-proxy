1ae637f990a57fc4cf0da8ceacd87aafb1b3a028
Inderpal <inderpalr2020@gmail.com>
2021-01-20 05:57:49 -0800

    trigger for build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Just a comment to trigger build on dockerhub
+#Just a comment to trigger build on dockerhubs
 set -e
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
